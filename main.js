4cf344fb07a0494c78d53768b0ed9ebfb346bd16
Ziyu Wang <68621123+wang-ziyu1@users.noreply.github.com>
2023-02-10 22:18:46 -0500

    Hover click and interface

--- main.js
+++ main.js
@@ -5,9 +5,16 @@
 function clickPoint(name){
 	let x = (parseInt(document.getElementById(name).getAttribute('cx')) - 30) / 40
 	let y = (430 - parseInt(document.getElementById(name).getAttribute('cy'))) / 40
-	document.getElementById("cord").innerHTML = "(" + x +","+ y+")";
+	document.getElementById("cord").innerHTML = "The point last clicked is at "+"(" + x +","+ y+")";
 	document.getElementById(name).classList.toggle("circle-click");
 }
 
-/*document.getElementsByTagName('circle')
-		.addEventListener('click', clickPoint);*/
+function addPoint(){
+x = document.getElementById("x_cord").options[document.getElementById("x_cord").selectedIndex].value;
+y = document.getElementById("y_cord").options[document.getElementById("y_cord").selectedIndex].value;
+document.getElementByDatasetName()
+}
+
+
+document.getElementById("Add")
+		.addEventListener('click', addPoint);
